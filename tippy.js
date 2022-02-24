3a73ebaac53a7a4dc1c71d83311c732dab1ed067
Eleven <elevenvac@pm.me>
2022-02-24 21:01:17 +0300

    beta page

---
+++ tippy.js
@@ -0,0 +1,20 @@
+tippy('#project-apptime', {
+    content: 'Apptime',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws'
+});
+
+tippy('#project-asuna', {
+    content: 'Asuna (closed)',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws'
+});
+
+tippy('#project-metro', {
+    content: 'Istanbul Rail Systems Archive',
+    animation: 'scale',
+    placement: 'bottom',
+    theme: 'ws'
+});
